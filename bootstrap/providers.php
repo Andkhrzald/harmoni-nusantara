f40f6d2c8fb84bbcb04193e71ac5feb3ebe6c8aa
Andkhrzald <Maxcyandkh20@gmail.com>
2026-05-17 18:32:53 +0700

    ambil update

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,11 +1,7 @@
 <?php
-
-use App\Filament\AdminPanel;
 use App\Providers\AppServiceProvider;
 use App\Providers\TitleServiceProvider;
-
 return [
     AppServiceProvider::class,
     TitleServiceProvider::class,
-    AdminPanel::class,
 ];
